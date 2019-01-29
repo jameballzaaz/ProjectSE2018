29ee6fd0151c35db50b50250269d99b920c7ae3f
jameballzaaz <jame-ball1@hotmail.com>
2019-01-29 12:41:01 +0700

    new

---
+++ originalProject/src/app/list/list.page.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { TourService, Tour } from '../tour.service';
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.page.html',
+  styleUrls: ['./list.page.scss']
+})
+export class ListPage implements OnInit {
+
+  tour : Tour[];
+
+  
+  
+  constructor(private tourService : TourService) { }
+
+  ngOnInit() {
+    this.tourService.gettour().subscribe(
+      res => {
+        this.tour = res;
+      }
+    );
+  }    
+}
+   
+
+
